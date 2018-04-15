59812683b2e03c9f0c02b60c98329f56ff56302c
radaeze <ramia.davis@gmail.com>
2018-04-15 00:24:13 +0000

    more fog fixes

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -18,7 +18,7 @@ class PictureUploader < CarrierWave::Uploader::Base
         # Configuration for Amazon S3
         provider:               'AWS',
         aws_access_key_id:      ENV['S3_ACCESS_KEY'],
-        aws_secret_access_key:  ENV['S3_SECRET_KEY']
+        aws_secret_access_key:  ENV['S3_SECRET_KEY'],
         region: 'us-west-1'
       }
       config.fog_directory     =  ENV['S3_BUCKET']
