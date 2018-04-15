4e5dc6a4bc1189257865ee99c3c650694c0450ff
radaeze <ramia.davis@gmail.com>
2018-04-15 00:29:47 +0000

    more fixes

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -11,20 +11,6 @@ class PictureUploader < CarrierWave::Uploader::Base
     storage :file
   end
   
-  if Rails.env.production?
-    CarrierWave.configure do |config|
-      config.fog_provider = 'fog/aws'    
-      config.fog_credentials = {
-        # Configuration for Amazon S3
-        provider:               'AWS',
-        aws_access_key_id:      ENV['S3_ACCESS_KEY'],
-        aws_secret_access_key:  ENV['S3_SECRET_KEY'],
-        region: 'us-west-1'
-      }
-      config.fog_directory     =  ENV['S3_BUCKET']
-  end
-end
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
