451ced25d974010c5a94589b5ea734ed5cdc5cc2
EC2 Default User <ec2-user@ip-172-31-17-94.us-west-2.compute.internal>
2018-04-29 23:00:17 +0000

    added pictures to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180416210703) do
+ActiveRecord::Schema.define(version: 20180426223826) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string "slug", null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20180416210703) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
