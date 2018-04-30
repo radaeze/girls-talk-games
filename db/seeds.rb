0dc0fdac6b405f9eda6d33237c04761ee66a8d4f
radaeze <33758858+radaeze@users.noreply.github.com>
2018-04-30 07:04:14 +0000

    fake users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
 
+50.times do |index|
+    pass = Faker::Name.unique.name
+    User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.unique.email,
+             password: pass, password_confirmation: pass, username: Faker::Pokemon.unique.name)
+    end
 User.create(first_name: "Ramia", last_name: "Davis", email: "ramia.davis@gmail.com", password: "1luvvash!",
                                    password_confirmation: "1luvvash!", username: "Admin")
                                    
 User.create(first_name: "Hello", last_name: "World", email: "ramdavis@mills.edu", password: "hellohello",
                                    password_confirmation: "hellohello", username: "TestUser")
+                                   
+                                
