20a835a8ceea5f5b8b0905e007f057ddba6eac54
EC2 Default User <ec2-user@ip-172-31-17-94.us-west-2.compute.internal>
2018-05-01 06:55:49 +0000

    added delete method to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 before_action :logged_in_user, only: [:create, :destroy]
+ before_action :correct_user,   only: :destroy
 
 
 
@@ -14,6 +15,9 @@ before_action :logged_in_user, only: [:create, :destroy]
   end
 
   def destroy
+     @post.destroy
+    flash[:success] = "Post deleted"
+    redirect_to request.referrer || root_url
   end
 
   private
